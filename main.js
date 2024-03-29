5cdd82258595600c54a6cdc06cbc9c07809e0e38
Gurmehar Singh <110217871+GURSV@users.noreply.github.com>
2024-03-29 11:41:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "ML Engineer", "Web Developer"],
+    strings: ["ML Engineer", "Data Analyst", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
